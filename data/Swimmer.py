870fda927b80be69388c2c69574f56c61cc4c70e
Robert Hou <robertyhou@gmail.com>
2019-04-23 19:33:26 -0700

    new data

---
+++ data/Swimmer.py
@@ -0,0 +1,7 @@
+class Swimmer:
+    def __init__(self, name, time, place):
+        self.name = name
+        self.competitions = {}
+
+    def add_comp(self, competition, lost_to):
+        self.competition[competition]
